dc716aa3dc112f9faf16adbddf97808772b0f335
alice-frln <alice.frelin@edu.univ-fcomte.fr>
2026-09-21 13:49:25 +0200

    mode statique pour deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,19 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import tailwindcss from '@tailwindcss/vite';
-
-import netlify from '@astrojs/netlify';
-
-const isBuild = process.argv.includes('build');
 
 // https://astro.build/config
 export default defineConfig({
-  output: isBuild ? 'server' : 'static',
-
   vite: {
     plugins: [tailwindcss()]
-  },
-
-  adapter: isBuild ? netlify() : undefined
+  }
 });
